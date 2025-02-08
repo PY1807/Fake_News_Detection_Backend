3fea24904b42c7e769c4c75349587bb0c7cefad4
PY1807 <pythehead23@gmail.com>
2025-02-08 18:15:14 +0530

    Update views.py

--- backend/analysepost/views.py
+++ backend/analysepost/views.py
@@ -1,37 +1,4 @@
-# from rest_framework import viewsets
-# from .models import User, Post, Hashtag
-# from .serializers import UserSerializer, PostSerializer, HashtagSerializer
-# from rest_framework.response import Response
-# from rest_framework.decorators import action
-# from restframework.decoratord import api_
-# import requests
-# from .utils import detect_fake_news
-# from .models import User,Post
 
-# class UserViewSet(viewsets.ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-
-# class PostViewSet(viewsets.ModelViewSet):
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
-
-#     @action(detail=True, methods=['post'])
-#     def check_fake_news(self, request, pk=None):
-#         post = self.get_object()
-#         response = requests.get(f'http://localhost:8000/api/posts')
-#         data = response.json()
-
-#         is_fake = detect_fake_news(data)
-
-#         post.is_fake = is_fake
-#         post.save()
-
-#         return Response({'postid': post.postid, 'is_fake': is_fake})
-
-# class HashtagViewSet(viewsets.ModelViewSet):
-#     queryset = Hashtag.objects.all()
-#     serializer_class = HashtagSerializer
 from .models import User,Post
 from django.shortcuts import render
 from .models import Signup,Post1
