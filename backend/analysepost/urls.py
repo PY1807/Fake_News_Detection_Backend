878684a081312f879ee66b9d505a81012b6a4693
PY1807 <pythehead23@gmail.com>
2025-02-11 02:03:14 +0530

    Update urls.py

--- backend/analysepost/urls.py
+++ backend/analysepost/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
-    path('', views.index, name="index"),  # 
+    path('', views.index, name="index"),
+      path('hashtags/',views.getpost_from_hashtag,name='getpost_from_hashtag'),  # 
     path('allposts/', views.get_all_posts, name="get_all_posts"),
     path('posts/create/', views.create_post, name="create_post"), 
+    path('login/',views.login_user,name="login_user"),
     path('signup/', views.signup_user, name="signup_user"), 
+    path('trendingposts/',views.get_trending_posts,name="get_trending_posts")
 ]
