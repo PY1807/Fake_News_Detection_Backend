6966ee5126c6a7fab8e38b15f1a03807f2677603
PY1807 <pythehead23@gmail.com>
2025-02-19 02:28:50 +0530

    Update urls.py

--- backend/analysepost/urls.py
+++ backend/analysepost/urls.py
@@ -5,13 +5,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-      path('hashtags/',views.getpost_from_hashtag,name='getpost_from_hashtag'),  # 
-    path('allposts/', views.get_all_posts, name="get_all_posts"),
-    path('posts/create/', views.create_post, name="create_post"), 
-    path('login/',views.login_user,name="login_user"),
-    path('auth/',views.auth,name="authentication"),
-    path('signup/', views.signup_user, name="signup_user"), 
-    path('trendingposts/',views.get_trending_posts,name="get_trending_posts"),
-    path('search/',views.search,name="search"),
-    path('verify/',views.verify,name="verify"),
+      path('hashtags',views.getpost_from_hashtag,name='getpost_from_hashtag'),  # 
+    path('allposts', views.get_all_posts, name="get_all_posts"),
+    path('posts/create', views.create_post, name="create_post"), 
+    path('login',views.login_user,name="login_user"),
+    path('auth',views.auth,name="authentication"),
+    path('signup', views.signup_user, name="signup_user"), 
+    path('trendingposts',views.get_trending_posts,name="get_trending_posts"),
+    path('search',views.search,name="search"),
+    path('verify',views.verify,name="verify"),
+    path('fake_posts',views.get_fake_posts,name="fake_posts")
 ]
